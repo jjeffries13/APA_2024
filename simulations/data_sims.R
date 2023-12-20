05b1b38d73f7f93b25930666504b3d640fe36478
Jay Jeffries <jayjeffries13@gmail.com>
2023-12-19 22:47:39 -0600

    exportData code

--- simulations/data_sims.R
+++ simulations/data_sims.R
@@ -1,6 +1,8 @@
-pacman::p_load(here, tidyverse, simsem, lavaan)
+pacman::p_load(here, tidyverse, simsem, lavaan, mice, skimr)
 
 # lavaan syntax version of {simsem} ----
+
+## population model ----
 
 pop_model <- "
 f1 =~ NA*i1 + 0.7*i1 + 0.7*i2 + 0.7*i3 + 0.7*i4 + 0.7*i5 + 
@@ -19,48 +21,137 @@ i9 ~~ 0.51*i9
 i10 ~~ 0.51*i10
 "
 
+## sample model ----
+
 analysis_model <- "
 f1 =~ NA*i1 + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10
 
 f1 ~~ 1*f1
+
+i1 ~ 1*1
+i2 ~ 1*1
+i3 ~ 1*1
+i4 ~ 1*1
+i5 ~ 1*1
+i6 ~ 1*1
+i7 ~ 1*1
+i8 ~ 1*1
+i9 ~ 1*1
+i10 ~ 1*1
 "
 
-sim_output_10pm <- sim(1000, analysis_model, n = 1000, generate = pop_model, 
-                       lavaanfun = "cfa", seed = 13131, multicore = T, pmMCAR = 0.10)
+# sim_output_10pm <- sim(nRep = 1000, model = analysis_model, n = 1000, 
+#                        generate = pop_model, lavaanfun = "cfa", generate = "lavaan",
+#                        seed = 13131, multicore = T, pmMCAR = 0.10)
 
-summary(sim_output_10pm)
-summaryFit(sim_output_10pm)
-summaryParam(sim_output_10pm)
-plotCutoff(sim_output_10pm, 0.05)
+# 1000 .dat datasets with 10% missingness (sim_data_10i_10pm) ----
 
-sim_output_25pm <- sim(1000, analysis_model, n = 1000, generate = pop_model, 
-                       lavaanfun = "cfa", seed = 13131, multicore = T, pmMCAR = 0.25)
+# exportData(nRep = 1000, model = analysis_model, n = 1000, program = "Mplus", 
+#            fileStem = "sim_10i_10pm", missCode = -99, seed = 13131, multicore = T, pmMCAR = 0.10)
 
-summary(sim_output_25pm)
-summaryFit(sim_output_25pm)
-summaryParam(sim_output_25pm)
-plotCutoff(sim_output_25pm, 0.05)
+# 1000 .dat datasets with 25% missingness (sim_data_10i_25pm) ----
 
-sim_output_50pm <- sim(1000, analysis_model, n = 1000, generate = pop_model, 
-                       lavaanfun = "cfa", seed = 13131, multicore = T, pmMCAR = 0.5)
+# exportData(nRep = 1000, model = analysis_model, n = 1000, program = "Mplus",
+#            fileStem = "sim_10i_25pm", missCode = -99, seed = 13131, multicore = T, pmMCAR = 0.25)
 
-summary(sim_output_50pm)
-summaryFit(sim_output_50pm)
-summaryParam(sim_output_50pm)
-plotCutoff(sim_output_50pm, 0.05)
+# 1000 .dat datasets with 50% missingness (sim_data_10i_50pm) ----
 
-sim_output_75pm <- sim(1000, analysis_model, n = 1000, generate = pop_model, 
-                       lavaanfun = "cfa", seed = 13131, multicore = T, pmMCAR = 0.75)
+# exportData(nRep = 1000, model = analysis_model, n = 1000, program = "Mplus",
+#            fileStem = "sim_10i_50pm", missCode = -99, seed = 13131, multicore = T, pmMCAR = 0.50)
 
-summary(sim_output_75pm)
-summaryFit(sim_output_75pm)
-summaryParam(sim_output_75pm)
-plotCutoff(sim_output_75pm, 0.05)
+# 1000 .dat datasets with 75% missingness (sim_data_10i_75pm) ----
 
-sim_output_90pm <- sim(1000, analysis_model, n = 1000, generate = pop_model, 
-                       lavaanfun = "cfa", seed = 13131, multicore = T, pmMCAR = 0.90)
+# exportData(nRep = 1000, model = analysis_model, n = 1000, program = "Mplus",
+#            fileStem = "sim_10i_75pm", missCode = -99, seed = 13131, multicore = T, pmMCAR = 0.75)
 
-summary(sim_output_90pm)
-summaryFit(sim_output_90pm)
-summaryParam(sim_output_90pm)
-plotCutoff(sim_output_90pm, 0.05)
+# 1000 .dat datasets with 90% missingness (sim_data_10i_90pm) ----
+
+# exportData(nRep = 1000, model = analysis_model, n = 1000, program = "Mplus",
+#            fileStem = "sim_10i_90pm", missCode = -99, seed = 13131, multicore = T, pmMCAR = 0.90)
+
+set.seed(13131) 
+
+sim_data_10i <- generate(model = analysis_model, n = 1000, maxDraw = 1000, empirical = T)
+
+skim(sim_data_10i)
+
+# 10% missing ----
+sim_data_10i_10pm <- sim_data_10i |>
+  ampute(prop = 0.10, mech = "MCAR")
+
+data_cond1 <- sim_data_10i_10pm$amp
+
+skim(data_cond1)
+
+# 25% missing ----
+sim_data_10i_25pm <- sim_data_10i |>
+  ampute(prop = 0.25, mech = "MCAR")
+
+data_cond2 <- sim_data_10i_25pm$amp
+
+skim(data_cond2)
+
+# 50% missing ----
+sim_data_10i_50pm <- sim_data_10i |>
+  ampute(prop = 0.5, mech = "MCAR")
+
+data_cond3 <- sim_data_10i_50pm$amp
+
+skim(data_cond3)
+
+# 75% missing ----
+sim_data_10i_75pm <- sim_data_10i |>
+  ampute(prop = 0.75, mech = "MCAR")
+
+data_cond4 <- sim_data_10i_75pm$amp
+
+skim(data_cond4)
+
+# 90% missing ----
+sim_data_10i_90pm <- sim_data_10i |>
+  ampute(prop = 0.90, mech = "MCAR")
+
+data_cond5 <- sim_data_10i_90pm$amp
+
+skim(data_cond5)
+
+# summary(sim_output_10pm)
+# summaryFit(sim_output_10pm)
+# summaryParam(sim_output_10pm)
+# plotCutoff(sim_output_10pm, 0.05)
+#
+# sim_output_25pm <- sim(nRep = 1000, model = analysis_model, n = 1000, 
+#                        generate = pop_model, lavaanfun = "cfa", 
+#                        seed = 13131, multicore = T, pmMCAR = 0.25)
+# 
+# # summary(sim_output_25pm)
+# # summaryFit(sim_output_25pm)
+# # summaryParam(sim_output_25pm)
+# # plotCutoff(sim_output_25pm, 0.05)
+# 
+# sim_output_50pm <- sim(nRep = 1000, model = analysis_model, n = 1000, 
+#                        generate = pop_model, lavaanfun = "cfa", 
+#                        seed = 13131, multicore = T, pmMCAR = 0.5)
+# 
+# # summary(sim_output_50pm)
+# # summaryFit(sim_output_50pm)
+# # summaryParam(sim_output_50pm)
+# # plotCutoff(sim_output_50pm, 0.05)
+# 
+# sim_output_75pm <- sim(nRep = 1000, model = analysis_model, n = 1000, 
+#                        generate = pop_model, lavaanfun = "cfa", 
+#                        seed = 13131, multicore = T, pmMCAR = 0.75)
+# 
+# # summary(sim_output_75pm)
+# # summaryFit(sim_output_75pm)
+# # summaryParam(sim_output_75pm)
+# # plotCutoff(sim_output_75pm, 0.05)
+# 
+# sim_output_90pm <- sim(nRep = 1000, model = analysis_model, n = 1000, 
+#                        generate = pop_model, lavaanfun = "cfa", 
+#                        seed = 13131, multicore = T, pmMCAR = 0.90)
+# 
+# # summary(sim_output_90pm)
+# # summaryFit(sim_output_90pm)
+# # summaryParam(sim_output_90pm)
+# # plotCutoff(sim_output_90pm, 0.05)
