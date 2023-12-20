3372c056659e0d2b69945a3a7eb647a07201e9eb
Jay Jeffries <jjeffries8@unl.edu>
2023-12-20 16:32:40 -0600

    25pm data merges

--- simulations/data_merge.R
+++ simulations/data_merge.R
@@ -1,6 +1,6 @@
-pacman::p_load(here, tidyverse, plyr, doParallel)
+pacman::p_load(here, tidyverse)
 
-cores <- detectCores()
+# 10 items 10% missing ----
 
 files_10i_10mp <- list.files(path = here("./data/sim_data_10i_10pm"), 
                     pattern = ".dat", recursive = T, full.names = T)
@@ -34,4 +34,59 @@ merged_data_10i_10mp <- merged_data_10i_10mp |>
   mutate(total = sum(c_across(c(X1:X10)), na.rm = T),
     mean = mean(c_across(c(X1:X10)), na.rm = T),
     n_complete = sum(!is.na(c_across(c(X1:X10)))),
-    prorated = total/n_missing)
+    prorated = total/n_complete)
+
+prorated_desc_10i_10mp <- merged_data_10i_10mp |>
+  group_by(dataset) |>
+  summarise(scale_mean = mean(c_across(c(X1:X10)), na.rm = T),
+            scale_sd = sd(c_across(c(X1:X10)), na.rm = T)) 
+
+overall_prorated_desc_10i_10mp <- prorated_desc_10i_10mp |> 
+  summarise(mean_avg = mean(scale_mean),
+            se_avg = (mean_avg / sqrt(nrow(merged_data_10i_10mp))),
+            ci_lower = (mean_avg - (1.96*se_avg)),
+            ci_upper = (mean_avg + (1.96*se_avg)))
+
+# 10 items 25% missing ----
+
+
+files_10i_25mp <- list.files(path = here("./data/sim_data_10i_25pm"), 
+                             pattern = ".dat", recursive = T, full.names = T)
+
+merged_data_10i_25mp <- read_delim(file = files_10i_25mp, col_names = F, delim = " ", na = "-99") |>
+  bind_rows()
+
+merged_data_10i_25mp <- merged_data_10i_25mp |>
+  mutate(id = rep(c(1:1000), times = 1000), # make case id (1:1000 per dataset)
+         dataset = rep(c(1:1000), each = 1000)) |> # make dataset id 
+  relocate(dataset, .before = 1) |>
+  relocate(id, .after = dataset)
+
+desc_10i_25mp <- merged_data_10i_25mp |>
+  group_by(dataset) |>
+  summarise(scale_mean = mean(c_across(c(X1:X10)), na.rm = T),
+            scale_sd = sd(c_across(c(X1:X10)), na.rm = T)) 
+
+overall_desc_10i_25mp <- desc_10i_25mp |>
+  summarise(mean_avg = mean(scale_mean),
+            se_avg = (mean_avg / sqrt(nrow(desc_10i_25mp))),
+            ci_lower = (mean_avg - (1.96*se_avg)),
+            ci_upper = (mean_avg + (1.96*se_avg)))
+
+merged_data_10i_25mp <- merged_data_10i_25mp |>
+  rowwise() |>
+  mutate(total = sum(c_across(c(X1:X10)), na.rm = T),
+         mean = mean(c_across(c(X1:X10)), na.rm = T),
+         n_complete = sum(!is.na(c_across(c(X1:X10)))),
+         prorated = total/n_complete)
+
+prorated_desc_10i_25mp <- merged_data_10i_25mp |>
+  group_by(dataset) |>
+  summarise(scale_mean = mean(c_across(c(X1:X10)), na.rm = T),
+            scale_sd = sd(c_across(c(X1:X10)), na.rm = T)) 
+
+overall_prorated_desc_10i_25mp <- prorated_desc_10i_25mp |> 
+  summarise(mean_avg = mean(scale_mean),
+            se_avg = (mean_avg / sqrt(nrow(merged_data_10i_25mp))),
+            ci_lower = (mean_avg - (1.96*se_avg)),
+            ci_upper = (mean_avg + (1.96*se_avg)))
